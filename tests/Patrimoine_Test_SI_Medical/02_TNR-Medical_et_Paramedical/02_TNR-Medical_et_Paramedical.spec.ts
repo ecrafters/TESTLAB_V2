bb6063cce00c0feef69e5343c3589050d22ee26c
Idiop <ibrahima.diop@eyone.net>
2026-04-08 17:05:40 +0000

    refactor: skip redundant queue-based test steps and implement new treatment, surveillance, and examination workflows for hospitalizations

--- tests/Patrimoine_Test_SI_Medical/02_TNR-Medical_et_Paramedical/02_TNR-Medical_et_Paramedical.spec.ts
+++ tests/Patrimoine_Test_SI_Medical/02_TNR-Medical_et_Paramedical/02_TNR-Medical_et_Paramedical.spec.ts
@@ -24,35 +24,35 @@ test.describe('TNR Medical et Paramedical', () => {
         let patientName: string;
         let hospitalName: string;
 
-        await test.step('TC-007 : Créer une prestation de type consultation depuis la file d\'attente', async () => {
+        await test.step.skip('TC-007 : Créer une prestation de type consultation depuis la file d\'attente', async () => {
             await loginWithCredentials(page, 'drsy@eyone.net', 'passe');  // Utilise automatiquement les identifiants de l'environnement
             // await page.locator('#vertical-menu-btn').click();
             await page.getByRole('link', { name: ' Prestations En attente' }).click();
             await createPEC(page, 'Consultation');
         });
 
-        await test.step('TC-008 : Créer une prestation de type analyse depuis la file d\'attente', async () => {
+        await test.step.skip('TC-008 : Créer une prestation de type analyse depuis la file d\'attente', async () => {
             await page.goBack();
             await createPEC(page, 'Analyse');
         });
 
-        await test.step('TC-009 : Créer une prestation de type radiologie depuis la file d\'attente', async () => {
+        await test.step.skip('TC-009 : Créer une prestation de type radiologie depuis la file d\'attente', async () => {
             await page.goBack();
             await createPEC(page, 'Radiologie');
         });
 
-        await test.step('TC : Créer une prestation de type hospitalisation depuis la file d\'attente', async () => {
+        await test.step.skip('TC : Créer une prestation de type hospitalisation depuis la file d\'attente', async () => {
             await page.goBack();
             await createPEC(page, 'Hospitalisation');
         });
 
-        await test.step('TC- : Créer une prestation de type ambulatoire depuis la file d\'attente', async () => {
+        await test.step.skip('TC- : Créer une prestation de type ambulatoire depuis la file d\'attente', async () => {
             await page.goBack();
             await createPEC(page, 'Ambulatoire');
         });
 
         await test.step('TC-010 : Créer une prestation médicale de type analyse depuis le menu prestation médicale', async () => {
-            // await loginWithCredentials(page, 'drsy@eyone.net', 'passe');  // Utilise automatiquement les identifiants de l'environnement
+            await loginWithCredentials(page, 'drsy@eyone.net', 'passe');  // Utilise automatiquement les identifiants de l'environnement
             await page.locator('#vertical-menu-btn').click();
             await getHospitalName(page).then((name: string) => hospitalName = name);
             await navigateToPatientsList(page);
@@ -67,24 +67,129 @@ test.describe('TNR Medical et Paramedical', () => {
         });
 
         await test.step('TC-011 : Créer une prestation médicale de type radiologie depuis le menu prestation médicale', async () => {
-            await page.waitForTimeout(500);
             await createPrestation(page, patientName, hospitalName, 'Imagerie');
         });
 
         await test.step('TC- : Créer une prestation médicale de type consultation depuis le menu prestation médicale', async () => {
-            await page.waitForTimeout(500);
             await createPrestation(page, patientName, hospitalName, 'Consultation');
         });
 
+        await test.step('TC- : Créer une prestation médicale de type ambulatoire depuis le menu prestation médicale', async () => {
+            await createPrestation(page, patientName, hospitalName, 'Ambulatoire');
+        });
+
         await test.step('TC- : Créer une prestation de type hospitalisation depuis le menu prestation', async () => {
-            await page.waitForTimeout(500);
             await createPrestation(page, patientName, hospitalName, 'Hospitalisation');
         });
 
-        await test.step('TC- : Créer une prestation médicale de type ambulatoire depuis le menu prestation médicale', async () => {
-            await page.waitForTimeout(500);
-            await createPrestation(page, patientName, hospitalName, 'Ambulatoire');
+        await test.step('TC- : Ajouter un traitement à une hospitalisation', async () => {
+            await page.getByRole('button', { name: 'Traitements ou soins' }).click();
+            await expect(page.getByRole('button', { name: ' Ajouter un traitement/soin' })).toBeVisible();
+            await page.getByRole('button', { name: ' Ajouter un traitement/soin' }).click();
+            await expect(page.getByText('Ajouter un traitment/soin')).toBeVisible();
+            const treatmentName = `Traitement Test ${faker.number.int(1000)}`;
+            await page.getByRole('textbox').first().fill(treatmentName);
+            await page.getByRole('combobox', { name: 'Rechercher un produit' }).pressSequentially('DOLI', { delay: 100 });;
+            await page.locator('span').filter({ hasText: 'Doliprane' }).first().click();
+            await page.getByRole('textbox').nth(1).fill('1 fois par jour pendant une semaine');
+            await page.locator('#dd').fill(faker.date.recent().toISOString().split('T')[0]);
+            await page.locator('#hd').fill(faker.date.recent().toISOString().split('T')[1].split(':')[0] + ':' + faker.date.recent().toISOString().split('T')[1].split(':')[1]);
+            await page.getByRole('button', { name: 'Enregistrer' }).click();
+            const treatmentRow = page.locator('tbody tr').filter({ hasText: treatmentName });
+            await expect(treatmentRow).toBeVisible();
+            await treatmentRow.locator('button').filter({ hasText: 'Visualiser' }).click();
+            await expect(page.getByText('Doliprane')).toBeVisible();
+            await expect(page.getByText('1 fois par jour pendant une semaine')).toBeVisible();
+            await page.getByText('Retour à la liste').click();
         });
+
+        await test.step('TC- : Effectuer la planification d\'un traitement', async () => {
+            await expect(page.getByText('Planifier un traitement/soin')).toBeVisible();
+            await page.getByText('Planifier un traitement/soin').click();
+            await page.getByText('Planifier un traitment/soin').click();
+            const planificationName = `Planification Test ${faker.number.int(1000)}`;
+            await page.locator('form').filter({ hasText: 'Description *' }).getByRole('textbox').fill(planificationName);
+            await page.getByRole('combobox', { name: 'Rechercher un produit' }).pressSequentially('DOLI', { delay: 100 });;
+            await page.locator('span').filter({ hasText: 'Doliprane' }).first().click();
+            await page.locator('form').filter({ hasText: 'MédicamentDétail Posologie' }).getByRole('textbox').fill('Une fois par jour');
+            await page.locator('#method').getByRole('combobox').click();
+            await page.getByRole('option', { name: 'abdou ndiaye' }).first().click();
+            await page.getByRole('listbox').filter({ hasText: 'Veuillez sélectionner une' }).getByRole('combobox').click();
+            await page.getByRole('option', { name: 'Unique' }).click();
+            await page.locator('#dd').fill(faker.date.recent().toISOString().split('T')[0]);
+            await page.locator('#hd').fill(faker.date.recent().toISOString().split('T')[1].split(':')[0] + ':' + faker.date.recent().toISOString().split('T')[1].split(':')[1]);
+            await page.getByRole('button', { name: 'Enregistrer' }).click();
+        });
+
+        await test.step('TC- : Effectuer la planification d\'un traitement', async () => {
+            await expect(page.getByText('Feuille de surveillance')).toBeVisible();
+            await page.getByText('Feuille de surveillance').click();
+            await expect(page.getByText('Ajouter une saisie')).toBeVisible();
+            await page.getByText('Ajouter une saisie').click();
+            await expect(page.getByText('Effectuer l\'examen')).toBeVisible();
+            await page.getByPlaceholder('JJ/MM/AAAA').nth(2).fill(faker.date.recent().toISOString().split('T')[0]);
+            await page.getByPlaceholder('JJ/MM/AAAA').nth(3).fill(faker.date.recent().toISOString().split('T')[1].split(':')[0] + ':' + faker.date.recent().toISOString().split('T')[1].split(':')[1]);
+            await expect(page.getByRole('heading', { name: 'Constantes' })).toBeVisible();
+            await page.locator('.d-flex.flex-column > .form-control').first().fill(faker.number.int(100).toString());
+            await page.locator('div:nth-child(2) > .d-flex > .form-control').first().fill(faker.number.int(100).toString());
+            await page.locator('div:nth-child(3) > div > .d-flex > .form-control').first().fill(faker.number.int(100).toString());
+            await page.locator('div:nth-child(3) > div:nth-child(2) > .d-flex > .form-control').fill(faker.number.int(100).toString());
+            await page.getByRole('button', { name: 'Enregistrer' }).click();
+            await expect(page.getByText('Feuille de surveillance')).toBeVisible();
+        });
+
+        await test.step('TC- : Effectuer la planification d\'une surveillance', async () => {
+            await expect(page.getByText('Planifier')).toBeVisible();
+            await page.getByText('Planifier').click();
+            await expect(page.getByText('Effectuer l\'examen')).toBeVisible();
+            await page.locator('#method').getByRole('combobox').click();
+            await page.getByRole('option', { name: 'abdou ndiaye' }).first().click();
+            await page.getByRole('listbox').filter({ hasText: 'Veuillez sélectionner une' }).getByRole('combobox').click();
+            await page.getByRole('option', { name: 'Unique' }).click();
+            await page.locator('#dd').fill(faker.date.recent().toISOString().split('T')[0]);
+            await page.locator('#hd').fill(faker.date.recent().toISOString().split('T')[1].split(':')[0] + ':' + faker.date.recent().toISOString().split('T')[1].split(':')[1]);
+            await page.getByRole('button', { name: 'Enregistrer' }).click();
+            await expect(page.getByText('Planifié', { exact: true })).toBeVisible();
+        });
+
+        await test.step('TC- : Ajouter un examen de type analyse dans une prestation de type hospitalisation', async () => {
+            await expect(page.getByText('Examens demandés')).toBeVisible();
+            await page.getByText('Examens demandés').click();
+            await expect(page.getByText('Ajouter un examen')).toBeVisible();
+            await page.getByText('Ajouter un examen').click();
+            await page.locator('div').filter({ hasText: /^Veuillez sélectionner un élément$/ }).first().click();
+            await page.locator('.ng-dropdown-panel .ng-option').filter({ hasText: 'Analyses' }).first().click();
+            await page.locator('div').filter({ hasText: /^Veuillez sélectionner un élément$/ }).nth(1).click();
+            await page.locator('.ng-dropdown-panel .ng-option').filter({ hasText: 'Sang - urine' }).first().click();
+            await expect(page.getByText('Bulletin d\'Analyses Médicales')).toBeVisible();
+            await page.locator('input[type="checkbox"]').first().check();
+            await page.locator('input[type="checkbox"]').nth(2).check();
+            await page.locator('input[type="checkbox"]').nth(3).check();
+            await page.getByText('Sauvegarder l\'examen').click();
+            await page.getByRole('button', { name: 'Sans télétransmission' }).click();
+        });
+
+        await test.step('TC- : Ajouter un examen de type analyse dans une prestation de type consultation', async () => {
+            await page.reload();
+            await page.locator('#vertical-menu-btn').click();
+            await createPrestation(page, patientName, hospitalName, 'Consultation');
+            await expect(page.getByText('Examens demandés')).toBeVisible();
+            await page.getByText('Examens demandés').click();
+            await expect(page.getByText('Ajouter un examen')).toBeVisible();
+            await page.getByText('Ajouter un examen').click();
+            await page.locator('div').filter({ hasText: /^Veuillez sélectionner un élément$/ }).first().click();
+            await page.locator('.ng-dropdown-panel .ng-option').filter({ hasText: 'Analyses' }).first().click();
+            await page.locator('div').filter({ hasText: /^Veuillez sélectionner un élément$/ }).nth(1).click();
+            await page.locator('.ng-dropdown-panel .ng-option').filter({ hasText: 'Sang - urine' }).first().click();
+            await expect(page.getByText('Bulletin d\'Analyses Médicales')).toBeVisible();
+            await page.locator('input[type="checkbox"]').first().check();
+            await page.locator('input[type="checkbox"]').nth(2).check();
+            await page.locator('input[type="checkbox"]').nth(3).check();
+            await page.getByText('Sauvegarder l\'examen').click();
+            await page.getByRole('button', { name: 'Sans télétransmission' }).click();
+            await page.pause();
+        });
+
     });
 
 });
